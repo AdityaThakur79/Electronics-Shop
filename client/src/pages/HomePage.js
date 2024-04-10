7ba7a960f823bc79f70398b6272c452e4412c505
Thakur Aditya <adityathakurbusiness@gmail.com>
2024-04-10 10:46:53 +0530

    Hero-Section Caraousel Implemented

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Home from "../components/layouts/Herosection/Home";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Home</h1>
+     <Home/>
     </div>
   );
 };
