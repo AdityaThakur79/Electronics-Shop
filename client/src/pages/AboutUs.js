2574581d3f2e0ef70bbb4673c439ee627b0d07a2
Thakur Aditya <adityathakurbusiness@gmail.com>
2024-11-17 13:16:53 +0530

    Deployment Ready

--- client/src/pages/AboutUs.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layout from "../components/layouts/Layouts.js";
-
-const AboutUs = () => {
-  return <Layout title={"About us - Ecommer app"}></Layout>;
-};
-
-export default AboutUs;
